29f8f23c2b821657bdae888324b6ec29f26416d7
Victor Samuel Oliveira <victorsamueloliveira59@gmail.com>
2025-05-23 15:49:13 -0300

    feat: criado classe DbContext

---
+++ src/IdentityHub.Infrastructure/IdentityHubContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using IdentityHub.Domain.Entities;
+
+namespace IdentityHub.Infrastructure;
+
+class IdentityHubContext : DbContext
+{
+     public IdentityHubContext(DbContextOptions<IdentityHubContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> User => Set<User>();
+    public virtual DbSet<Role> Role => Set<Role>();
+    public virtual DbSet<UserRole> UserRole => Set<UserRole>()  ;
+    public virtual DbSet<LoginHistory> LoginHistory => Set<LoginHistory>();
+}
