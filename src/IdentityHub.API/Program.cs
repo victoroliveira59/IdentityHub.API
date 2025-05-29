e88e06fbf158bdadbd600bf617731c7cbd57ac12
Victor Samuel Oliveira <victorsamueloliveira59@gmail.com>
2025-05-28 22:32:52 -0300

    feat: Adicionado service de DbContext

--- src/IdentityHub.API/Program.cs
+++ src/IdentityHub.API/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using IdentityHub.Infrastructure;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<IdentityHubDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 
